f45fe6a70d08588868a162a2d3e9a763327e9025
khojayev12 <khojayev12@gmail.com>
2025-01-28 10:06:31 +0500

    header change

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,12 @@
+import { FaRegHeart } from "react-icons/fa6";
 import '../App.css';
 import {Link} from "react-router-dom";
 
 function Header() {
     return (
-        <div className="header w-full bg-blue-500 px-20 py-5 flex-row justify-between">
-            <div className="header-logo text-3xl  inline-block  text-black ">Exclusive</div>
-            <div className="header-navigation text-3xl inline-block flex-row">
+        <div className="header w-full  px-20 py-5 flex-row flex justify-between bg-blue-300">
+            <div className="header-logo text-3xl  inline-block  text-black bg-red-300 ">Exclusive</div>
+            <div className="header-navigation text-3xl inline-block flex-row bg-yellow-300">
                 <div className="header-nav-single text-xl inline-block mx-5 text-black" >
                     <Link to="/">Home</Link>
                 </div>
@@ -19,7 +20,7 @@ function Header() {
                     <Link to="/signup">Sign Up</Link>
                 </div>
             </div>
-            <div className="header-right inline-block" >right</div>
+            <div className="header-right inline-block bg-green-300" ><FaRegHeart className="inline-block" /></div>
         </div>
     );
 }
