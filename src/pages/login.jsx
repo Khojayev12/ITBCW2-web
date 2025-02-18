382cbfc8e88c36dffbc5d6912cf00e329235d17c
khojayev12 <khojayev12@gmail.com>
2025-02-18 10:36:48 +0500

    Login/Sign Up updated

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,8 +1,12 @@
 import '../App.css';
 import {Link} from "react-router-dom";
 import Logo from '../media/login.png'
+import {useState} from "react";
+import CustomInput from "../components/customInput";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     return (
         <div className="page login w-full  px-20 py-5 h-full flex justify-center items-center">
             <div className="login-left w-8/12 h-full ">
@@ -10,10 +14,8 @@ function Login() {
             </div>
             <div className="login-right w-4/12  h-full inline-flex justify-center items-center flex-col">
                 <div className="login-title text-3xl block">Login to Exclusive</div>
-                <input type="email" placeholder="email"
-                       className="block h-10 text-l p-3 border-c-button2 text-c-button2 border-2 outline-0 rounded-sm w-8/12 my-3 placeholder:text-gray-500"/>
-                <input type="password" placeholder="password"
-                       className="block h-10 text-l p-3 border-c-button2 text-c-button2 border-2 outline-0 rounded-sm w-8/12 my-3 placeholder:text-gray-500"/>
+                <CustomInput className="w-8/12 " placeholder="Email" content={email} setContent={setEmail}/>
+                <CustomInput className="w-8/12 " type="password" placeholder="Password" content={password} setContent={setPassword}/>
                 <button
                     className="login-btn flex h-10 text-l p-3 bg-c-button2 text-white rounded-sm w-8/12 my-3 items-center justify-center hover:bg-c-hover-button2">Login
                 </button>
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,8 +1,14 @@
 import '../App.css';
 import {Link} from "react-router-dom";
 import Logo from '../media/login.png'
+import CustomInput from "../components/customInput";
+import {useState} from "react";
 
 function Signup() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [name, setName] = useState("");
     return (
         <div className="page login w-full  px-20 py-5 h-full flex justify-center items-center">
             <div className="login-left w-8/12 h-full ">
@@ -10,16 +16,10 @@ function Signup() {
             </div>
             <div className="login-right w-4/12  h-full inline-flex justify-center items-center flex-col">
                 <div className="login-title text-3xl block mb-4">Sign Up to Exclusive</div>
-                <input type="tet" placeholder="name"
-                       className="block h-10 text-l p-3 border-c-button2 text-c-button2 border-2 outline-0 rounded-sm w-8/12 my-3 placeholder:text-gray-500"/>
-                <input type="email" placeholder="email"
-                       className="block h-10 text-l p-3 border-c-button2 text-c-button2 border-2 outline-0 rounded-sm w-8/12 my-3 placeholder:text-gray-500"/>
-                <input type="address" placeholder="address"
-                       className="block h-10 text-l p-3 border-c-button2 text-c-button2 border-2 outline-0 rounded-sm w-8/12 my-3 placeholder:text-gray-500"/>
-                <input type="password" placeholder="password"
-                       className="block h-10 text-l p-3 border-c-button2 text-c-button2 border-2 outline-0 rounded-sm w-8/12 my-3 placeholder:text-gray-500"/>
-                <input type="password" placeholder="re-enter password"
-                       className="block h-10 text-l p-3 border-c-button2 text-c-button2 border-2 outline-0 rounded-sm w-8/12 my-3 placeholder:text-gray-500"/>
+                <CustomInput className="w-8/12 " placeholder="Name" content={name} setContent={setName}/>
+                <CustomInput className="w-8/12 " placeholder="Email" content={email} setContent={setEmail}/>
+                <CustomInput className="w-8/12 " placeholder="Password" content={password} setContent={setPassword}/>
+                <CustomInput className="w-8/12 " placeholder="Confirm Password" content={confirmPassword} setContent={setConfirmPassword}/>
                 <button
                     className="login-btn flex h-10 text-l p-3 bg-c-button2 text-white rounded-sm w-8/12 my-3 items-center justify-center hover:bg-c-hover-button2">
                     Sign Up
