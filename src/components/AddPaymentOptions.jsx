f63aea5490f6f86a1ebeae9d4be7ded7898c85e3
khojayev12 <khojayev12@gmail.com>
2025-02-18 10:12:16 +0500

    payment options component done

--- src/components/AddPaymentOptions.jsx
+++ src/components/AddPaymentOptions.jsx
@@ -1,38 +1,47 @@
 import '../App.css';
 import CustomInput from "./customInput";
 import {useState} from "react";
+import VisaLogo from "../media/VisaLogo.png"
 
 function AddPaymentOptions(props) {
-    const [district, setDistrict] = useState("");
-    const [city, setCity] = useState("");
-    const [addressline1, setAddressline1] = useState("");
-    const [addressline2, setAddressline2] = useState("");
+    const [type, setType] = useState("");
+    const [cardNumber, setCardNumber] = useState("");
+    const [cardCVV, setCardCVV] = useState("");
+    const [cardExpireDT, setCardExpireDT] = useState("");
 
     const HandleChanges = ()=>{
-        var newAddressList = props.address;
-        newAddressList[props.addressID] = {"district":district, "city":city, "addressline1":addressline1, "addressline2":addressline2};
-        props.setAddress(newAddressList);
+        var newPaymentOptionsList = props.pOptions;
+        newPaymentOptionsList[props.paymentOptionID] = {"type":type, "cardNumber":cardNumber, "cardCVV":cardCVV, "cardExpireDT":cardExpireDT};
+        props.setpOptions(newPaymentOptionsList);
+    }
+
+    const HandleTypeChanges = (newType)=>{
+        setType(newType);
     }
     return (
         <div className="bg-white w-full h-fit" onChange={HandleChanges} >
-            <div className="w-full flex justify-around items-center my-4 mx-0">
-                <div className="w-5/12 inline-flex flex-col">
-                    <div className="text-black text-lg">District</div>
-                    <CustomInput className="w-full " placeholder="District" content={district} setContent={setDistrict}/>
+            <div>
+                <div className="w-fit mx-2 flex flex-row">
+                    <input type="radio" id="opt-1" value="visa" name="payment-option" className="inline-block" onChange={()=>HandleTypeChanges("visa")} />
+                    <img src={VisaLogo} alt="" className="w-12 inline-block mr-4"/>
+                    <input type="radio" id="opt-2" value="mastercard" name="payment-option" className="inline-block" onChange={()=>HandleTypeChanges("mastercard")} />
+                    <img src={VisaLogo} alt="" className="w-12 inline-block mr-4"/>
                 </div>
-                <div className="w-5/12 inline-flex flex-col">
-                    <div className="text-black text-lg">City</div>
-                    <CustomInput className="w-full " placeholder="City" content={city} setContent={setCity}/>
+            </div>
+            <div className="w-full flex justify-around items-start my-4 mx-0">
+                <div className="w-11/12 inline-flex flex-col">
+                    <div className="text-black text-lg">Card Number</div>
+                    <CustomInput className="w-full " placeholder="Card Number" content={cardNumber} setContent={setCardNumber}/>
                 </div>
             </div>
             <div className="w-full flex justify-around items-center my-4 mx-0">
                 <div className="w-5/12 inline-flex flex-col">
-                    <div className="text-black text-lg">Address line 1</div>
-                    <CustomInput className="w-full " placeholder="Address line 1" content={addressline1} setContent={setAddressline1}/>
+                    <div className="text-black text-lg">Expire Date</div>
+                    <CustomInput className="w-full " type="text" placeholder="MM/YY" content={cardExpireDT} setContent={setCardExpireDT}/>
                 </div>
                 <div className="w-5/12 inline-flex flex-col">
-                    <div className="text-black text-lg">Address line 2</div>
-                    <CustomInput className="w-full " placeholder="Address line 2" content={addressline2} setContent={setAddressline2}/>
+                    <div className="text-black text-lg">CVV</div>
+                    <CustomInput className="w-full " placeholder="CVV" content={cardCVV} setContent={setCardCVV}/>
                 </div>
             </div>
         </div>
--- src/components/paymentOptions.jsx
+++ src/components/paymentOptions.jsx
@@ -3,6 +3,7 @@ import CustomInput from "./customInput";
 import {useState} from "react";
 import AddAddress from "./addAddress";
 import CustomButton from "./customButton";
+import AddPaymentOptions from "./AddPaymentOptions";
 
 function PaymentOptions(props) {
     const [pOptions, setpOptions] = useState([{}, {}, {}, {}]);
@@ -10,7 +11,14 @@ function PaymentOptions(props) {
     return (
         <div
             className="AdressBook m-auto px-20 py-10 min-w-[870px] h-fit border border-white rounded drop-shadow bg-white">
-            payment options
+            <div className="text-c-button2 text-xl font-semibold">Payment Option 1</div>
+            <AddPaymentOptions pOptions={pOptions} setpOptions={setpOptions} paymentOptionID={1} />
+            <div className="text-c-button2 text-xl font-semibold">Payment Option 2</div>
+            <AddPaymentOptions pOptions={pOptions} setpOptions={setpOptions} paymentOptionID={2} />
+            <div className="text-c-button2 text-xl font-semibold">Payment Option 3</div>
+            <AddPaymentOptions pOptions={pOptions} setpOptions={setpOptions} paymentOptionID={3} />
+            <div className="text-c-button2 text-xl font-semibold">Payment Option 4</div>
+            <AddPaymentOptions pOptions={pOptions} setpOptions={setpOptions} paymentOptionID={4} />
             <div className="flex justify-end items-center" >
                 <button className="flex  text-lg  bg-white text-black rounded-sm px-8 py-4 h-14 items-center justify-center mx-4 hover:bg-[#f5f5f5]" >Cancel</button>
                 <CustomButton className="my-3" >Save Changes</CustomButton>
