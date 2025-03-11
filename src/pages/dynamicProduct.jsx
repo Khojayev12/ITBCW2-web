f8d6b1255f1ffe77da15729f8ca8e52b57699564
khojayev12 <khojayev12@gmail.com>
2025-03-11 10:55:26 +0500

    product page  added

--- src/pages/dynamicProduct.jsx
+++ src/pages/dynamicProduct.jsx
@@ -12,20 +12,23 @@ function DynamicProduct() {
                     <div className={"product-photos w-2/3"} >photo</div>
                     <div className={"product-info w-1/3 ml-4"} >
                         <div className={"p-title text-2xl text-black font-semibold"} >Havic HV G-92 Gamepad</div>
-                        <div className={"text-lg my-1 text-gray-500 flex flex-row"}>
+                        <div className={"text-lg my-4 text-gray-500 flex flex-row"}>
                             <ProductReview score={4}/> (150 Reviews) | {state?<span className={"text-lg text-green-500 opacity-60"} >In Storck</span>:<span  className={"text-lg text-red-500 opacity-60"}>Out of Storck</span>}
                         </div>
-                        <div className={"p-cost  my-1 text-2xl text-black"}>$192.0</div>
-                        <div className={"p-description  my-1 text-xs"}>PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.</div>
+                        <div className={"p-cost  my-4 text-2xl text-black"}>$192.0</div>
+                        <div className={"p-description  my-1 text-lg"}>PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.</div>
                         <hr/>
-                        <div className={"text-xl  my-1"} >
+                        <div className={"text-xl  my-4"} >
                             Colors: <input type="radio"/>
                         </div>
-                        <div className={"text-xl  my-1 flex flex-row align-middle"}>
+                        <div className={"text-xl  my-4 flex flex-row align-middle"}>
                             Storage:
                             <div className={"inline-block ml-4 h-8 px-2 rounded text-center text-lg " + " bg-c-button2 text-white"}>128GB</div>
                             <div className={"inline-block ml-4 h-8 px-2 rounded border border-gray-500 text-lg text-center"}>256GB</div>
                             <div className={"inline-block ml-4 h-8 px-2 rounded border border-gray-500 text-lg text-center"}>512GB</div>
+                        </div>
+                        <div className={"my-4 flex flex-row"} >
+                            <div className={"border border-gray-500 roun"}></div>
                         </div>
                     </div>
                 </div>
